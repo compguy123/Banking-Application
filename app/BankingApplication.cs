5a230372da2d3c689e4efc45b42e9db28717a115
compguy123 <77632039+compguy123@users.noreply.github.com>
2023-06-02 17:32:59 -0500

    inital files

---
+++ app/BankingApplication.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string [] args){
+            Console.WriteLine("Hello world!");
+
+
+
+        int [] nums = new int[25];
+
+
+        bool breaks= false;
+        for(int i=0; i<nums.Length && breaks==false;i++ ){
+
+            if(nums[i]==10){
+                breaks=true;
+            }
+        }
+
+
+
+
+        }
+    }
+}
---
+++ app/Client.cs
@@ -0,0 +1,25 @@
+class Client
+{
+
+    private string fName;
+    private string lName;
+    private int sin;
+    private int pNum;
+    private int amount;
+
+
+     public Client(string firstName, string lastName, int socialInsuranceNumber, int phoneNumber, int initialAmount)
+    {
+        fName = firstName;
+        lName = lastName;
+        sin = socialInsuranceNumber;
+        pNum = phoneNumber;
+        amount = initialAmount;
+    }
+
+    public void display(){
+
+        
+    }
+
+}
